b2d3d901099086c38b79335c289bc6930644c167
Nath_ <firminchanteux@gmail.com>
2021-02-10 17:18:03 +0100

    add functions to Memeber

--- Classes/Member.php
+++ Classes/Member.php
@@ -12,9 +12,7 @@ class Member extends Mailchimp {
 
     public function getMember(string $listID) {
 
-        $url = str_replace('{list_id}', $listID, $this->_base);
-
-        $members = $this->cURL($url)
+        $members = $this->cURL($this->getEndpoint($listID))
         ->setEncoding(self::APPLICATION_JSON)
         ->setOptions([CURLOPT_USERPWD => $this->getUserPwd()])
         ->setDebug(...$this->dd())
@@ -25,23 +23,55 @@ class Member extends Mailchimp {
 
     public function getMembers(string $listID, string $email) {
 
-        $url = str_replace(['{list_id}', '{subscriber_hash}'], [$listID, md5($email)], $this->_base);
-
-        $member = $this->cURL($url)
+        $member = $this->cURL($this->getEndpoint($listID, $email))
         ->setEncoding(self::APPLICATION_JSON)
         ->setOptions([CURLOPT_USERPWD => $this->getUserPwd()])
         ->setDebug(...$this->dd())
         ->get();
 
         return json_decode($member);
-
     }
 
-    public function create() {
+    public function create(string $listID, array $values) {
 
+        $create = $this->cURL($this->getEndpoint($listID))
+        ->setEncoding(self::APPLICATION_JSON)
+        ->setOptions([CURLOPT_USERPWD => $this->getUserPwd()])
+        ->setDebug(...$this->dd())
+        ->post($values);
+
+        return json_decode($create);
     }
 
-    public function update() {}
+    public function update(string $listID, string $email, array $values) {
 
-    public function replace() {}
+        $update = $this->cURL($this->getEndpoint($listID, $email))
+        ->setEncoding(self::APPLICATION_JSON)
+        ->setOptions([CURLOPT_USERPWD => $this->getUserPwd()])
+        ->setDebug(...$this->dd())
+        ->patch($values);
+
+        return json_decode($update);
+    }
+
+    public function replace(string $listID, string $email, array $values) {
+
+        $replace = $this->cURL($this->getEndpoint($listID, $email))
+        ->setEncoding(self::APPLICATION_JSON)
+        ->setOptions([CURLOPT_USERPWD => $this->getUserPwd()])
+        ->setDebug(...$this->dd())
+        ->put($values);
+
+        return json_decode($replace);
+    }
+
+    #region Getter
+
+    public function getEndpoint(string $listID, string $email = '') : string {
+        return !empty($email)
+        ? str_replace(['{list_id}', '{subscriber_hash}'], [$listID, md5($email)], $this->_base . '/{subscriber_hash}')
+        : str_replace('{list_id}', $listID, $this->_base);
+    }
+
+    #endregion
 }
