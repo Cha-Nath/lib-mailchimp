0a8c5a1e3ad99114ae172a0d24995772c719a03f
Nath_ <firminchanteux@gmail.com>
2022-03-22 11:57:42 +0100

    init new feature

--- Classes/Mailchimp.php
+++ Classes/Mailchimp.php
@@ -2,46 +2,91 @@
 
 namespace Nlib\Mailchimp\Classes;
 
-use nlib\cURL\Interfaces\cURLConstantInterface;
-use nlib\cURL\Traits\cURLTrait;
+use MailchimpMarketing\Api\AccountExportApi;
+use MailchimpMarketing\Api\AccountExportsApi;
+use MailchimpMarketing\Api\ActivityFeedApi;
+use MailchimpMarketing\Api\AuthorizedAppsApi;
+use MailchimpMarketing\Api\AutomationsApi;
+use MailchimpMarketing\Api\BatchWebhooksApi;
+use MailchimpMarketing\Api\BatchesApi;
+use MailchimpMarketing\Api\CampaignFoldersApi;
+use MailchimpMarketing\Api\CampaignsApi;
+use MailchimpMarketing\Api\ConnectedSitesApi;
+use MailchimpMarketing\Api\ConversationsApi;
+use MailchimpMarketing\Api\CustomerJourneysApi;
+use MailchimpMarketing\Api\EcommerceApi;
+use MailchimpMarketing\Api\FacebookAdsApi;
+use MailchimpMarketing\Api\FileManagerApi;
+use MailchimpMarketing\Api\LandingPagesApi;
+use MailchimpMarketing\Api\ListsApi;
+use MailchimpMarketing\Api\PingApi;
+use MailchimpMarketing\Api\ReportingApi;
+use MailchimpMarketing\Api\ReportsApi;
+use MailchimpMarketing\Api\RootApi;
+use MailchimpMarketing\Api\SearchCampaignsApi;
+use MailchimpMarketing\Api\SearchMembersApi;
+use MailchimpMarketing\Api\TemplateFoldersApi;
+use MailchimpMarketing\Api\TemplatesApi;
+use MailchimpMarketing\Api\VerifiedDomainsApi;
+
 use nlib\Instance\Traits\InstanceTrait;
-use nlib\Log\Traits\DebugTrait;
 use nlib\Log\Traits\LogTrait;
-use Nlib\Mailchimp\Interfaces\MailchimpInterface;
 use nlib\Missing\Exceptions\MissingException;
 use nlib\Missing\Traits\MissingTrait;
 use nlib\Yaml\Traits\ParserTrait;
 
-abstract class Mailchimp implements MailchimpInterface, cURLConstantInterface {
+use Nlib\Mailchimp\Interfaces\MailchimpInterface;
+
+class Mailchimp extends \MailchimpMarketing\Configuration implements MailchimpInterface {
 
     use InstanceTrait;
-    use cURLTrait;
     use LogTrait;
-    use DebugTrait;
     use MissingTrait;
     use ParserTrait;
 
-    protected $_base = 'https://{server}.api.mailchimp.com/3.0';
+    private $_AccountExport = null;
+    private $_AccountExports = null;
+    private $_ActivityFeed = null;
+    private $_AuthorizedApps = null;
+    private $_Automations = null;
+    private $_BatchWebhooks = null;
+    private $_Batches = null;
+    private $_CampaignFolders = null;
+    private $_Campaigns = null;
+    private $_ConnectedSites = null;
+    private $_Conversations = null;
+    private $_CustomerJourneys = null;
+    private $_Ecommerce = null;
+    private $_FacebookAds = null;
+    private $_FileManager = null;
+    private $_LandingPages = null;
+    private $_Lists = null;
+    private $_Ping = null;
+    private $_Reporting = null;
+    private $_Reports = null;
+    private $_Root = null;
+    private $_SearchCampaigns = null;
+    private $_SearchMembers = null;
+    private $_TemplateFolders = null;
+    private $_Templates = null;
+    private $_VerifiedDomains = null;
 
-    private $_user = 'user';
-    private $_pwd;
-    private $_server;
-    
-    abstract public function getEndpoint(string $listID, string $var = '');
+    public function __construct() {
+
+        $this->tempFolderPath = sys_get_temp_dir();
+        // $this->init(Path::i($this->_i())->getConfig() . 'config');
+    }
 
     public function init(string $config) : self {
 
-        $configs = $this->Parser()->get($config);
         try {
+            
+            $parameters = $this->Parser()->get($config);
 
-            if(!$this->is_missing(['user', 'pwd', 'server'], $configs))
-                throw new MissingException($this->getMissings(), __CLASS__ . '::' . __FUNCTION__);
+            if(!$this->is_missing(['apiKey', 'server'], $parameters))
+                throw new MissingException($this->getMissings(), $this->l());
         
-            $this->setUser($configs['user'])
-            ->setPwd($configs['pwd'])
-            ->setServer($configs['server']);
-
-            $this->_base = str_replace('{server}', $this->getServer(), $this->_base);
+            $this->setConfig($parameters);
 
         } catch(MissingException $MissingException) {
             $this->dlog([$MissingException->getThrow() => json_encode($MissingException->getMissings())]);
@@ -49,21 +94,32 @@ abstract class Mailchimp implements MailchimpInterface, cURLConstantInterface {
 
         return $this;
     }
+    
+    public function AccountExport() : AccountExportApi { if(empty($this->_AccountExport)) $this->_AccountExport = new AccountExportApi($this); return $this->_AccountExport; }
+    public function AccountExports() : AccountExportsApi { if(empty($this->_AccountExports)) $this->_AccountExports = new AccountExportsApi($this); return $this->_AccountExports; }
+    public function ActivityFeed() : ActivityFeedApi { if(empty($this->_ActivityFeed)) $this->_ActivityFeed = new ActivityFeedApi($this); return $this->_ActivityFeed; }
+    public function AuthorizedApps() : AuthorizedAppsApi { if(empty($this->_AuthorizedApps)) $this->_AuthorizedApps = new AuthorizedAppsApi($this); return $this->_AuthorizedApps; }
+    public function Automations() : AutomationsApi { if(empty($this->_Automations)) $this->_Automations = new AutomationsApi($this); return $this->_Automations; }
+    public function BatchWebhooks() : BatchWebhooksApi { if(empty($this->_BatchWebhooks)) $this->_BatchWebhooks = new BatchWebhooksApi($this); return $this->_BatchWebhooks; }
+    public function Batches() : BatchesApi { if(empty($this->_Batches)) $this->_Batches = new BatchesApi($this); return $this->_Batches; }
+    public function CampaignFolders() : CampaignFoldersApi { if(empty($this->_CampaignFolders)) $this->_CampaignFolders = new CampaignFoldersApi($this); return $this->_CampaignFolders; }
+    public function Campaigns() : CampaignsApi { if(empty($this->_Campaigns)) $this->_Campaigns = new CampaignsApi($this); return $this->_Campaigns; }
+    public function ConnectedSites() : ConnectedSitesApi { if(empty($this->_ConnectedSites)) $this->_ConnectedSites = new ConnectedSitesApi($this); return $this->_ConnectedSites; }
+    public function Conversations() : ConversationsApi { if(empty($this->_Conversations)) $this->_Conversations = new ConversationsApi($this); return $this->_Conversations; }
+    public function CustomerJourneys() : CustomerJourneysApi { if(empty($this->_CustomerJourneys)) $this->_CustomerJourneys = new CustomerJourneysApi($this); return $this->_CustomerJourneys; }
+    public function Ecommerce() : EcommerceApi { if(empty($this->_Ecommerce)) $this->_Ecommerce = new EcommerceApi($this); return $this->_Ecommerce; }
+    public function FacebookAds() : FacebookAdsApi { if(empty($this->_FacebookAds)) $this->_FacebookAds = new FacebookAdsApi($this); return $this->_FacebookAds; }
+    public function FileManager() : FileManagerApi { if(empty($this->_FileManager)) $this->_FileManager = new FileManagerApi($this); return $this->_FileManager; }
+    public function LandingPages() : LandingPagesApi { if(empty($this->_LandingPages)) $this->_LandingPages = new LandingPagesApi($this); return $this->_LandingPages; }
+    public function Lists() : ListsApi { if(empty($this->_Lists)) $this->_Lists = new ListsApi($this); return $this->_Lists; }
+    public function Ping() : PingApi { if(empty($this->_Ping)) $this->_Ping = new PingApi($this); return $this->_Ping; }
+    public function Reporting() : ReportingApi { if(empty($this->_Reporting)) $this->_Reporting = new ReportingApi($this); return $this->_Reporting; }
+    public function Reports() : ReportsApi { if(empty($this->_Reports)) $this->_Reports = new ReportsApi($this); return $this->_Reports; }
+    public function Root() : RootApi { if(empty($this->_Root)) $this->_Root = new RootApi($this); return $this->_Root; }
+    public function SearchCampaigns() : SearchCampaignsApi { if(empty($this->_SearchCampaigns)) $this->_SearchCampaigns = new SearchCampaignsApi($this); return $this->_SearchCampaigns; }
+    public function SearchMembers() : SearchMembersApi { if(empty($this->_SearchMembers)) $this->_SearchMembers = new SearchMembersApi($this); return $this->_SearchMembers; }
+    public function TemplateFolders() : TemplateFoldersApi { if(empty($this->_TemplateFolders)) $this->_TemplateFolders = new TemplateFoldersApi($this); return $this->_TemplateFolders; }
+    public function Templates() : TemplatesApi { if(empty($this->_Templates)) $this->_Templates = new TemplatesApi($this); return $this->_Templates; }
+    public function VerifiedDomains() : VerifiedDomainsApi { if(empty($this->_VerifiedDomains)) $this->_VerifiedDomains = new VerifiedDomainsApi($this); return $this->_VerifiedDomains; }
 
-    #region Getter
-
-    public function getUser() : string { return $this->_user; }
-    public function getPwd() : string { return $this->_pwd; }
-    public function getServer() : string { return $this->_server; }
-    public function getUserPwd() : string { return $this->_user . ':' . $this->_pwd; }
-
-    #endregion
-
-    #region Setter
-
-    public function setUser(string $user) : self { $this->_user = $user; return $this; }
-    public function setPwd(string $pwd) : self { $this->_pwd = $pwd; return $this; }
-    public function setServer(string $server) : self { $this->_server = $server; return $this; }
-
-    #endregion
 }
--- Interfaces/MailchimpInterface.php
+++ Interfaces/MailchimpInterface.php
@@ -2,6 +2,33 @@
 
 namespace Nlib\Mailchimp\Interfaces;
 
+use MailchimpMarketing\Api\AccountExportApi;
+use MailchimpMarketing\Api\AccountExportsApi;
+use MailchimpMarketing\Api\ActivityFeedApi;
+use MailchimpMarketing\Api\AuthorizedAppsApi;
+use MailchimpMarketing\Api\AutomationsApi;
+use MailchimpMarketing\Api\BatchWebhooksApi;
+use MailchimpMarketing\Api\BatchesApi;
+use MailchimpMarketing\Api\CampaignFoldersApi;
+use MailchimpMarketing\Api\CampaignsApi;
+use MailchimpMarketing\Api\ConnectedSitesApi;
+use MailchimpMarketing\Api\ConversationsApi;
+use MailchimpMarketing\Api\CustomerJourneysApi;
+use MailchimpMarketing\Api\EcommerceApi;
+use MailchimpMarketing\Api\FacebookAdsApi;
+use MailchimpMarketing\Api\FileManagerApi;
+use MailchimpMarketing\Api\LandingPagesApi;
+use MailchimpMarketing\Api\ListsApi;
+use MailchimpMarketing\Api\PingApi;
+use MailchimpMarketing\Api\ReportingApi;
+use MailchimpMarketing\Api\ReportsApi;
+use MailchimpMarketing\Api\RootApi;
+use MailchimpMarketing\Api\SearchCampaignsApi;
+use MailchimpMarketing\Api\SearchMembersApi;
+use MailchimpMarketing\Api\TemplateFoldersApi;
+use MailchimpMarketing\Api\TemplatesApi;
+use MailchimpMarketing\Api\VerifiedDomainsApi;
+
 interface MailchimpInterface {
 
     /**
@@ -9,44 +36,162 @@ interface MailchimpInterface {
      * @param string $config
      * @return self
      */
-    public function init(string $configs);
-
+    public function init(string $config);
+    
     /**
      *
-     * @return string
+     * @return AccountExportApi
      */
-    public function getUser() : string;
-
+    public function AccountExport() : AccountExportApi;
+    
     /**
      *
-     * @return string
+     * @return AccountExportsApi
      */
-    public function getPwd() : string;
-
+    public function AccountExports() : AccountExportsApi;
+    
     /**
      *
-     * @return string
+     * @return ActivityFeedApi
      */
-    public function getServer() : string;
-
+    public function ActivityFeed() : ActivityFeedApi;
+    
     /**
      *
-     * @param string $user
-     * @return self
+     * @return AuthorizedAppsApi
      */
-    public function setUser(string $user);
-
+    public function AuthorizedApps() : AuthorizedAppsApi;
+    
     /**
      *
-     * @param string $pwd
-     * @return self
+     * @return AuthorizedAppsApi
      */
-    public function setPwd(string $pwd);
-
+    public function Automations() : AutomationsApi;
+    
     /**
      *
-     * @param string $server
-     * @return self
+     * @return AuthorizedAppsApi
      */
-    public function setServer(string $server);
+    public function BatchWebhooks() : BatchWebhooksApi;
+    
+    /**
+     *
+     * @return AuthorizedAppsApi
+     */
+    public function Batches() : BatchesApi;
+    
+    /**
+     *
+     * @return AuthorizedAppsApi
+     */
+    public function CampaignFolders() : CampaignFoldersApi;
+    
+    /**
+     *
+     * @return AuthorizedAppsApi
+     */
+    public function Campaigns() : CampaignsApi;
+    
+    /**
+     *
+     * @return AuthorizedAppsApi
+     */
+    public function ConnectedSites() : ConnectedSitesApi;
+    
+    /**
+     *
+     * @return AuthorizedAppsApi
+     */
+    public function Conversations() : ConversationsApi;
+    
+    /**
+     *
+     * @return AuthorizedAppsApi
+     */
+    public function CustomerJourneys() : CustomerJourneysApi;
+    
+    /**
+     *
+     * @return AuthorizedAppsApi
+     */
+    public function Ecommerce() : EcommerceApi;
+    
+    /**
+     *
+     * @return AuthorizedAppsApi
+     */
+    public function FacebookAds() : FacebookAdsApi;
+    
+    /**
+     *
+     * @return AuthorizedAppsApi
+     */
+    public function FileManager() : FileManagerApi;
+    
+    /**
+     *
+     * @return AuthorizedAppsApi
+     */
+    public function LandingPages() : LandingPagesApi;
+    
+    /**
+     *
+     * @return AuthorizedAppsApi
+     */
+    public function Lists() : ListsApi;
+    
+    /**
+     *
+     * @return AuthorizedAppsApi
+     */
+    public function Ping() : PingApi;
+    
+    /**
+     *
+     * @return AuthorizedAppsApi
+     */
+    public function Reporting() : ReportingApi;
+    
+    /**
+     *
+     * @return AuthorizedAppsApi
+     */
+    public function Reports() : ReportsApi;
+    
+    /**
+     *
+     * @return AuthorizedAppsApi
+     */
+    public function Root() : RootApi;
+    
+    /**
+     *
+     * @return AuthorizedAppsApi
+     */
+    public function SearchCampaigns() : SearchCampaignsApi;
+    
+    /**
+     *
+     * @return AuthorizedAppsApi
+     */
+    public function SearchMembers() : SearchMembersApi;
+    
+    /**
+     *
+     * @return AuthorizedAppsApi
+     */
+    public function TemplateFolders() : TemplateFoldersApi;
+    
+    /**
+     *
+     * @return AuthorizedAppsApi
+     */
+    public function Templates() : TemplatesApi;
+    
+    /**
+     *
+     * @return AuthorizedAppsApi
+     */
+    public function VerifiedDomains() : VerifiedDomainsApi;
+
 }
